6a79a2b16b8b733af5c0b260f75ee2f5cd28ab6c
SAArnol <seinaarnold02@gmail.com>
2025-04-26 15:52:01 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,17 @@
 function updateRates() {
-  document.getElementById('usdBuy').innerText = document.getElementById('usdBuyInput').value || 3546;
-  document.getElementById('usdSell').innerText = document.getElementById('usdSellInput').value || 3795;
-  // You can add more updates for GBP, EUR, etc here
+  const currencies = ['usd', 'gbp', 'eur', 'kes', 'tzs', 'zar', 'cny', 'rwf'];
+
+  currencies.forEach(currency => {
+    const buyInput = document.getElementById(`${currency}BuyInput`);
+    const sellInput = document.getElementById(`${currency}SellInput`);
+    const buyCell = document.getElementById(`${currency}Buy`);
+    const sellCell = document.getElementById(`${currency}Sell`);
+
+    if (buyInput.value) {
+      buyCell.innerText = buyInput.value;
+    }
+    if (sellInput.value) {
+      sellCell.innerText = sellInput.value;
+    }
+  });
 }
